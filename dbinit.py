8831f05ef109cb4247d12aa9b5fb9c3e0fe926f7
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-05-16 22:17:44 +0200

    small updates to db schemas

--- dbinit.py
+++ dbinit.py
@@ -28,12 +28,12 @@ table_queries = [
     # these can be submitted from slimerjstester.js for example
     # they might include screenshot(s) (if so the screenshot value
     # here will refer to an insert_id in the screenshots table)
-    'CREATE TABLE IF NOT EXISTS regression_results (id INT AUTO_INCREMENT, site INT, ua INT, engine TINYTEXT, date TIMESTAMP, bug_id TINYTEXT, result TEXT, screenshot INT, PRIMARY KEY(id))',
+    'CREATE TABLE IF NOT EXISTS regression_results (id INT AUTO_INCREMENT, data_set INT, site INT, ua INT, engine TINYTEXT, date TIMESTAMP, bug_id TINYTEXT, result TEXT, screenshot INT, PRIMARY KEY(id))',
     # test data table
     # TO EVALUATE: is it better to add fields for each plugin? Yes, with the drawback that adding plugins becomes harder..
     # perhaps a mixed solution where every key-value that has a key in the table is inserted, rest is JSON stringified
     # and added to plugin_data.
-    'CREATE TABLE IF NOT EXISTS test_data (id INT AUTO_INCREMENT, data_set INT, site INT, ua INT, engine TINYTEXT, state TINYINT(1), failing_because TEXT, hasHandheldFriendlyMeta TINYINT(1), hasViewportMeta TINYINT(1), hasMobileOptimizedMeta TINYINT(1), mobileLinkOrScriptUrl TINYINT(1), hasVideoTags TINYINT(1), pageWidthFitsScreen TINYINT(1), hasHtmlOrBodyMobileClass TINYINT(1), `iscroll` TINYINT(1), `link-prerender` TINYINT(1), `m3u8-links` TINYINT(1), `m3u8-videos` TINYINT(1), `mobify-check` TINYINT(1), `modernizr-at-media` TINYINT(1), `old-brightcove` TINYINT(1), `sencha-touch` TINYINT(1), `window-orientation-usage` TINYINT(1), `wptouch-check` TINYINT(1), other_plugin_data TEXT, PRIMARY KEY(id))',
+    'CREATE TABLE IF NOT EXISTS test_data (id INT AUTO_INCREMENT, data_set INT, site INT, ua INT, engine TINYTEXT, hostname TINYTEXT, state TINYINT(1), failing_because TEXT, hasHandheldFriendlyMeta TINYINT(1), hasViewportMeta TINYINT(1), hasMobileOptimizedMeta TINYINT(1), mobileLinkOrScriptUrl TINYINT(1), hasVideoTags TINYINT(1), pageWidthFitsScreen TINYINT(1), hasHtmlOrBodyMobileClass TINYINT(1), `iscroll` TINYINT(1), `link-prerender` TINYINT(1), `m3u8-links` TINYINT(1), `m3u8-videos` TINYINT(1), `mobify-check` TINYINT(1), `modernizr-at-media` TINYINT(1), `old-brightcove` TINYINT(1), `sencha-touch` TINYINT(1), `window-orientation-usage` TINYINT(1), `wptouch-check` TINYINT(1), other_plugin_data TEXT, PRIMARY KEY(id))',
     # a table of contact points - filled through automated discovery of contact forms,
     # social media accounts etc
     # Note: this is for "public" data only - not intended for dev E-mail addresses found on GitHub or LinkedIN..
